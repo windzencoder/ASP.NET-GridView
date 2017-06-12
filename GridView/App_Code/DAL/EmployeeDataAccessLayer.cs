40f6369fa444abc4f3dcec6031ed7774a7637e6e
windfred <2099260054@qq.com>
2017-06-12 23:20:34 +0800

    Deleting data from gridview using objectdatasource control - Part 15

---
+++ GridView/App_Code/DAL/EmployeeDataAccessLayer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+
+/// <summary>
+/// EmployeeDataAccessLayer 的摘要说明
+/// </summary>
+
+namespace GridView
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+    }
+
+    public class EmployeeDataAccessLayer
+    {
+        public EmployeeDataAccessLayer()
+        {
+            //
+            // TODO: 在此处添加构造函数逻辑
+            //
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> listEmployees = new List<Employee>();
+            string CS = ConfigurationManager.ConnectionStrings["SampleConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from tblEmployee", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.EmployeeId = Convert.ToInt32(rdr["EmployeeId"]);
+                    employee.Name = rdr["Name"].ToString();
+                    employee.Gender = rdr["Gender"].ToString();
+                    employee.City = rdr["City"].ToString();
+
+                    listEmployees.Add(employee);
+                }
+            }
+
+            return listEmployees;
+        }
+
+        //删除Employee
+        public static void DeleteEmployee(int EmployeeId)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["SampleConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("delete from tblEmployee where EmployeeId = @EmployeeId",con);
+                SqlParameter param = new SqlParameter("@EmployeeId", EmployeeId);
+                cmd.Parameters.Add(param);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ GridView/Part15.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Part15.aspx.cs" Inherits="Part15" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <%--注意添加DataKeyNames--%>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="ObjectDataSource1" BackColor="White" BorderColor="#3366CC" 
+            BorderStyle="None" BorderWidth="1px" CellPadding="4" DataKeyNames="EmployeeId">
+            <Columns>
+                <asp:CommandField ShowDeleteButton="True" />
+                <asp:BoundField DataField="EmployeeId" HeaderText="EmployeeId" SortExpression="EmployeeId" />
+                <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                <asp:BoundField DataField="Gender" HeaderText="Gender" SortExpression="Gender" />
+                <asp:BoundField DataField="City" HeaderText="City" SortExpression="City" />
+            </Columns>
+            <FooterStyle BackColor="#99CCCC" ForeColor="#003399" />
+            <HeaderStyle BackColor="#003399" Font-Bold="True" ForeColor="#CCCCFF" />
+            <PagerStyle BackColor="#99CCCC" ForeColor="#003399" HorizontalAlign="Left" />
+            <RowStyle BackColor="White" ForeColor="#003399" />
+            <SelectedRowStyle BackColor="#009999" Font-Bold="True" ForeColor="#CCFF99" />
+            <SortedAscendingCellStyle BackColor="#EDF6F6" />
+            <SortedAscendingHeaderStyle BackColor="#0D4AC4" />
+            <SortedDescendingCellStyle BackColor="#D6DFDF" />
+            <SortedDescendingHeaderStyle BackColor="#002876" />
+        </asp:GridView>
+        <asp:ObjectDataSource ID="ObjectDataSource1" runat="server" SelectMethod="GetAllEmployees" TypeName="GridView.EmployeeDataAccessLayer" DeleteMethod="DeleteEmployee">
+            <DeleteParameters>
+                <asp:Parameter Name="EmployeeId" Type="Int32" />
+            </DeleteParameters>
+        </asp:ObjectDataSource>
+    
+    </div>
+    </form>
+</body>
+</html>
